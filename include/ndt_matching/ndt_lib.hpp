f32fee2759b556787dcb4bfafa5b7e2b769c0511
woookey <woo.barczyk@gmail.com>
2019-08-28 20:54:04 +0100

    start implementing ndt_lib

--- include/ndt_matching/ndt_lib.hpp
+++ include/ndt_matching/ndt_lib.hpp
@@ -2,6 +2,11 @@
 #define NDT_MATCHING__NDT_LIB_HPP_
 
 #include "ndt_matching/visibility_control.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
+#include <memory>
 
 namespace ndt_matching
 {
@@ -10,8 +15,15 @@ class NdtLib
 {
 public:
   NdtLib();
-
   virtual ~NdtLib();
+
+  bool is_map_registered() const {return map_registered;}
+  void register_map(pcl::PointCloud<pcl::PointXYZ>& point_cloud);
+  geometry_msgs::msg::PoseStamped execute(pcl::PointCloud<pcl::PointXYZ> &lidar_point_cloud,
+		  geometry_msgs::msg::PoseStamped &pose_guess);
+private:
+  bool map_registered = false;
+  pcl::PointCloud<pcl::PointXYZ> point_cloud_;
 };
 
 }  // namespace ndt_matching
--- src/ndt_lib.cpp
+++ src/ndt_lib.cpp
@@ -11,4 +11,29 @@ NdtLib::~NdtLib()
 {
 }
 
+void NdtLib::register_map(pcl::PointCloud<pcl::PointXYZ>& point_cloud)
+{
+	point_cloud_ = point_cloud;
+	map_registered = true;
+}
+
+geometry_msgs::msg::PoseStamped NdtLib::execute(pcl::PointCloud<pcl::PointXYZ> &lidar_point_cloud,
+		  geometry_msgs::msg::PoseStamped &pose_guess)
+{
+	geometry_msgs::msg::PoseStamped estimate;
+	(void)lidar_point_cloud;
+	(void)pose_guess;
+
+	estimate.header = pose_guess.header;
+	estimate.pose.position.x = 1;
+	estimate.pose.position.y = 2;
+	estimate.pose.position.z = 10;
+	estimate.pose.orientation.w = 0.1;
+	estimate.pose.orientation.x = 0.2;
+	estimate.pose.orientation.y = 0.3;
+	estimate.pose.orientation.z = 0.4;
+
+	return estimate;
+}
+
 }  // namespace ndt_matching
